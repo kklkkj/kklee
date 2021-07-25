d0e60caebba8e32bc0dcc0baad233bf8e8051dd5
kklkkj <43706274+kklkkj@users.noreply.github.com>
2021-07-25 13:04:54 +0100

    Rename "Move to body" button

--- src/main.nim
+++ src/main.nim
@@ -50,7 +50,7 @@ afterUpdateRightBoxBody = proc(fx: int) =
         )
         rerender()
       se.appendChild shapeTableCell("",
-          createBonkButton("Move to body", moveToBody)
+          createBonkButton("Move to platform", moveToBody)
         )
       if fixture.fxShape.shapeType == stypePo:
         proc editVerticies =
