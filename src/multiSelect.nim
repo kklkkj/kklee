033194b9ef43382e204faafd4ce025521d59649d
kklkkj <43706274+kklkkj@users.noreply.github.com>
2021-07-29 11:11:27 +0100

    Move multiselect features to separate functions

--- src/multiSelect.nim
+++ src/multiSelect.nim
@@ -47,6 +47,118 @@ proc prop(name: string; field: VNode): VNode =
     text name
     field
 
+proc shapeMultiSelectEdit: VNode = buildHtml tdiv(
+    style = "display: flex; flex-flow: column".toCss):
+
+  ul(style = "font-size:11px; padding-left: 10px; margin: 3px".toCss):
+    li text "Shift+click shape name fields to select shapes"
+    li text """Note: you will have to reselect the platform to see changes
+made by multiselect"""
+    li text """Variables: x is current value, i is index in list of
+selected shapes (the first shape you selected will have i=0, the next one
+i=1, i=2, etc)"""
+    li text "Arithmetic, such as x*2+50, will be evaluated"
+
+  var appliers: seq[(int, var MapFixture) -> void]
+
+  template floatProp(name: string; mapFxProp: untyped): untyped =
+    var inp {.global.}: string = "x"
+    appliers.add proc (i: int; fx {.inject.}: var MapFixture) =
+      theEvaluator.addVars {"x": mapFxProp, "i": i.float}
+      var res = theEvaluator.eval inp
+      res = res.clamp(-1e6, 1e6)
+      if res == NaN: res = 0
+      mapFxProp = res
+    buildHtml:
+      prop name, bonkInput(inp, proc(parserInput: string): string =
+        theEvaluator.addVars {"x": 0.0, "i": 0.0}
+        discard theEvaluator.eval parserInput
+        return parserInput
+      , nil, s=>s)
+
+  template boolProp(name: string; mapFxProp: untyped): untyped =
+    var inp {.global.}: boolPropValue
+    appliers.add proc(i: int; fx {.inject.}: var MapFixture) =
+      case inp
+      of bpFalse: mapFxProp = false
+      of bpTrue: mapFxProp = true
+      of bpSame: discard
+    buildHtml:
+      prop name, tfsCheckbox(inp)
+
+  template colourChanger: untyped =
+    var
+      canChange {.global.} = false
+      inp {.global.}: int = 0
+    appliers.add proc(i: int; fx: var MapFixture) =
+      if canChange:
+        fx.f = inp
+    buildHtml tdiv(style =
+      "display:flex; flex-flow: row wrap; justify-content: space-between"
+      .toCss
+      ):
+      checkbox(canChange)
+      colourInput(inp)
+
+  floatProp("x", fx.fxShape.c.x)
+  floatProp("y", fx.fxShape.c.y)
+  floatProp("Angle (radians)", fx.fxShape.a)
+  floatProp("Rect width", fx.fxShape.bxW)
+  floatProp("Rect height", fx.fxShape.bxH)
+  floatProp("Circle radius", fx.fxShape.ciR)
+  floatProp("Density", fx.de)
+  floatProp("Bounciness", fx.re)
+  floatProp("Friction", fx.fr)
+
+  boolProp("No physics", fx.np)
+  boolProp("No grapple", fx.ng)
+  boolProp("Death", fx.d)
+
+  prop("Colour", colourChanger())
+
+  bonkButton "Apply", proc =
+    for i, f in selectedFixtures.mpairs:
+      for a in appliers: a(i, f)
+    saveToUndoHistory()
+    updateRenderer(true)
+    updateRightBoxBody(-1)
+
+proc shapeMultiSelectMove: VNode = buildHtml tdiv(
+    style = "display: flex; flex-flow: column".toCss):
+
+  var moveBody {.global.}: MapBody
+  select(style = "margin-top: 10px".toCss):
+    for bi in mapObject.physics.bro:
+      option:
+        text bi.getBody.n
+
+    proc onInput(e: Event; n: VNode) =
+      moveBody =
+        mapObject.physics.bro[e.target.OptionElement.selectedIndex].getBody
+    proc onMouseEnter(e: Event; n: VNode) =
+      moveBody =
+        mapObject.physics.bro[e.target.OptionElement.selectedIndex].getBody
+
+  bonkButton("Move to platform", proc =
+    block:
+      var i = 0
+      while i < fixturesBody.fx.len:
+        let fxid = fixturesBody.fx[i]
+        if fxid.getFx in selectedFixtures:
+          moveBody.fx.add fxid
+          fixturesBody.fx.delete i
+        else:
+          inc i
+
+    setCurrentBody(mapObject.physics.bodies.find moveBody)
+    fixturesBody = moveBody
+    updateLeftBox()
+    updateRenderer(true)
+    updateRightBoxBody(-1)
+    saveToUndoHistory()
+
+  , moveBody.isNil)
+
 proc shapeMultiSelect*: VNode =
   if getCurrentBody().getBody != fixturesBody:
     selectedFixtures = @[]
@@ -54,78 +166,8 @@ proc shapeMultiSelect*: VNode =
   fixturesBody = getCurrentBody().getBody
 
   buildHtml(tdiv(style = "display: flex; flex-flow: column".toCss)):
-    ul(style = "font-size:11px; padding-left: 10px; margin: 3px".toCss):
-      li text "Shift+click shape name fields to select shapes"
-      li text """Note: you will have to reselect the platform to see changes
- made by multiselect"""
-      li text """Variables: x is current value, i is index in list of
- selected shapes (the first shape you selected will have i=0, the next one
- i=1, i=2, etc)"""
-      li text "Arithmetic, such as x*2+50, will be evaluated"
+    shapeMultiSelectEdit()
 
-    var appliers: seq[(int, var MapFixture) -> void]
-
-    template floatProp(name: string; mapFxProp: untyped): untyped =
-      var inp {.global.}: string = "x"
-      appliers.add proc (i: int; fx {.inject.}: var MapFixture) =
-        theEvaluator.addVars {"x": mapFxProp, "i": i.float}
-        var res = theEvaluator.eval inp
-        res = res.clamp(-1e6, 1e6)
-        if res == NaN: res = 0
-        mapFxProp = res
-      buildHtml:
-        prop name, bonkInput(inp, proc(parserInput: string): string =
-          theEvaluator.addVars {"x": 0.0, "i": 0.0}
-          discard theEvaluator.eval parserInput
-          return parserInput
-        , nil, s=>s)
-
-    template boolProp(name: string; mapFxProp: untyped): untyped =
-      var inp {.global.}: boolPropValue
-      appliers.add proc(i: int; fx {.inject.}: var MapFixture) =
-        case inp
-        of bpFalse: mapFxProp = false
-        of bpTrue: mapFxProp = true
-        of bpSame: discard
-      buildHtml:
-        prop name, tfsCheckbox(inp)
-
-    template colourChanger: untyped =
-      var
-        canChange {.global.} = false
-        inp {.global.}: int = 0
-      appliers.add proc(i: int; fx: var MapFixture) =
-        if canChange:
-          fx.f = inp
-      buildHtml tdiv(style =
-        "display:flex; flex-flow: row wrap; justify-content: space-between"
-        .toCss
-        ):
-        checkbox(canChange)
-        colourInput(inp)
-
-    floatProp("x", fx.fxShape.c.x)
-    floatProp("y", fx.fxShape.c.y)
-    floatProp("Angle (radians)", fx.fxShape.a)
-    floatProp("Rect width", fx.fxShape.bxW)
-    floatProp("Rect height", fx.fxShape.bxH)
-    floatProp("Circle radius", fx.fxShape.ciR)
-    floatProp("Density", fx.de)
-    floatProp("Bounciness", fx.re)
-    floatProp("Friction", fx.fr)
-
-    boolProp("No physics", fx.np)
-    boolProp("No grapple", fx.ng)
-    boolProp("Death", fx.d)
-
-    prop("Colour", colourChanger())
-
-    bonkButton "Apply", proc =
-      for i, f in selectedFixtures.mpairs:
-        for a in appliers: a(i, f)
-      saveToUndoHistory()
-      updateRenderer(true)
-      updateRightBoxBody(-1)
     bonkButton "Delete shapes", proc =
       for f in selectedFixtures:
         let fxid = moph.fixtures.find f
@@ -136,38 +178,8 @@ proc shapeMultiSelect*: VNode =
       updateRenderer(true)
       updateRightBoxBody(-1)
 
-    var moveBody {.global.}: MapBody
-    select(style = "margin-top: 10px".toCss):
-      for bi in mapObject.physics.bro:
-        option:
-          text bi.getBody.n
+    shapeMultiSelectMove()
 
-      proc onInput(e: Event; n: VNode) =
-        moveBody =
-          mapObject.physics.bro[e.target.OptionElement.selectedIndex].getBody
-      proc onMouseEnter(e: Event; n: VNode) =
-        moveBody =
-          mapObject.physics.bro[e.target.OptionElement.selectedIndex].getBody
-
-    bonkButton("Move to platform", proc =
-      block:
-        var i = 0
-        while i < fixturesBody.fx.len:
-          let fxid = fixturesBody.fx[i]
-          if fxid.getFx in selectedFixtures:
-            moveBody.fx.add fxid
-            fixturesBody.fx.delete i
-          else:
-            inc i
-
-      setCurrentBody(mapObject.physics.bodies.find moveBody)
-      fixturesBody = moveBody
-      updateLeftBox()
-      updateRenderer(true)
-      updateRightBoxBody(-1)
-      saveToUndoHistory()
-
-    , moveBody.isNil)
 
 
 var
