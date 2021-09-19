99c973728481afb4f1c4786c6c1edccb1decbbeb
kklkkj <43706274+kklkkj@users.noreply.github.com>
2021-09-19 21:56:00 +0100

    Change some styles

--- src/mapBackups.nim
+++ src/mapBackups.nim
@@ -4,9 +4,9 @@ import
   kkleeApi
 
 proc mapBackupLoader*: VNode = buildHtml tdiv(
-    style = "display: flex; flex-flow: column; font-size: 15px".toCss):
-  proc backupOption(b: MapBackupObject): VNode = buildHtml li(
-    style = "margin-top: 5px; background-color: peru".toCss
+    style = "display: flex; flex-flow: column; font-size: 13px".toCss):
+  proc backupOption(b: MapBackupObject): VNode = buildHtml tdiv(
+    style = "padding: 5px 0px; border-top: 2px grey solid".toCss
   ):
     text b.getBackupLabel()
     proc onClick =
@@ -18,6 +18,5 @@ proc mapBackupLoader*: VNode = buildHtml tdiv(
       updateRightBoxBody(-1)
       updateWarnings()
 
-  ul(style = "padding-left: 15px; font-size: 13px".toCss):
-    for i in countdown(kkleeApi.mapBackups.high, 0):
-      backupOption(kkleeApi.mapBackups[i])
+  for i in countdown(kkleeApi.mapBackups.high, 0):
+    backupOption(kkleeApi.mapBackups[i])
--- src/mapSizeInfo.nim
+++ src/mapSizeInfo.nim
@@ -4,11 +4,10 @@ import
   kkleeApi
 
 proc mapSizeInfo*: VNode = buildHtml tdiv(
-    style = "display: flex; flex-flow: column; font-size: 15px".toCss):
-  ul:
-    li text &"Shapes: {moph.fixtures.len}/1000"
-    li text &"Platforms: {moph.bodies.len}/300"
-    li text &"Joints: {moph.joints.len}/100"
-    li text &"Spawns: {mapObject.spawns.len}/100"
-    li text &"Capzones: {mapObject.capZones.len}/50"
-    li text &"Data limit: {dataLimitInfo()}"
+    style = "display: flex; flex-flow: column; font-size: 16px".toCss):
+  span text &"Shapes: {moph.fixtures.len}/1000"
+  span text &"Platforms: {moph.bodies.len}/300"
+  span text &"Joints: {moph.joints.len}/100"
+  span text &"Spawns: {mapObject.spawns.len}/100"
+  span text &"Capzones: {mapObject.capZones.len}/50"
+  span text &"Data limit: {dataLimitInfo()}"
