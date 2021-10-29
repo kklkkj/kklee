624a8faff2071882be0385069777fe87e9f3b6af
Excigma <30280397+Excigma@users.noreply.github.com>
2021-10-30 12:05:33 +1300

    fix: onchange -> oninput for image opacity also set value -> defaultValue

--- src/editorImageOverlay.nim
+++ src/editorImageOverlay.nim
@@ -8,7 +8,6 @@ proc editorImageOverlay*: VNode = buildHtml tdiv(
   label(`for` = "kkleeEditorImageOverlayInput"):
     span text "Select files:"
   input(
-    class = "brownButton brownButton_classic buttonShadow",
     `type` = "file",
     id = "kkleeEditorImageOverlayInput",
     accept="image/*"):
@@ -18,13 +17,14 @@ proc editorImageOverlay*: VNode = buildHtml tdiv(
     span text "Opacity:"
   input(
     id = "kkleeEditorImageOverlayOpacity",
+    title = "Overlay opacity",
     class = "compactSlider compactSlider_classic",
     `type` = "range",
     min = "0",
     max = "1",
     step = "0.05",
-    value = "0.75", # This is not working :/
+    defaultValue = "0.75", # This is not working :/
     title = "Opacity"):
-      proc onchange(ev: Event; n: VNode) =
+      proc oninput(ev: Event; n: VNode) =
         editorImageOverlayOpacity = parseFloat(n.value)
         drawEditorImageOverlay()
